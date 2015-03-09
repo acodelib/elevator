caa21d096a3017000240f4d7afcae9a57212936e
acodelib <andrei_gurguta@yahoo.com>
2015-03-09 11:57:37 +0000

    Update elevator_class_builds.py

--- elevator_class_builds.py
+++ elevator_class_builds.py
@@ -3,39 +3,55 @@ from random import *
 
 #===========================Customer class=====================================================================================================================
 class Customer(object):
-    def __init__(self,SomeName,MaxFloors:'int',CurrentFloor:'int'):
+    #---------------------------------------------------------------------------------
+    def __init__(self,SomeName,MaxFloors:'int'):
         '''
         Customer will receive a name at instantiation: This Could be either a number or int (index);
-        MaxFloors and urrentFloor will be passed by environment (Building)
+        MaxFloors will be passed by environment (Building)
         '''
         self.Name        = ''
-        self.Destination = ''
-        self.Location    = ''
+        self.Destination = 0
+        self.Location    = 0
+        self.Direction   = ''
         self.Name        = SomeName
-#check if passed arguments are of valid types -- useful for only when Customer is used outside of a building:
+        #check if passed arguments are of valid types -- useful for only when Customer is used outside of a building:
         try:
             self.Destination = randint(0,int(MaxFloors)) #Customer decides it's own destination (and not the building for him)
         except ValueError:
             self.Destination = randint(0,120)
+            MaxFloors = 120
             print("Error trapped and solved: Destination of Customer can be only a valid Integer; Random values were assigned for instance (Customer {})".format(self.Name))
         try:
-            self.Location = int(CurrentFloor)
-        except ValueError:
+            self.Location = randint(0,int(MaxFloors)) #Customer decides what floor he will be waiting for the elevator
+        except ValueError:            
             self.Location = randint(0,120)
+            while self.Location == self.Destination:
+                self.Location = randint(0,120)           
             print("Error trapped and solved: Current floor Location of Customer can be only a valid Integer; Random values were assigned for instance (Customer {})".format(self.Name))
+        if self.Location < self.Destination:
+            self.Direction = 'up'
+        else:
+            self.Direction = 'down'
+    #---------------------------------------------------------------------------------            
     def __str__(self):
-        self.__Printable = '{} is sitting at floor {} and wants to reach floor {}'.format(self.Name,self.Location,self.Destination)
-        return self.__Printable
+        self.__Printable = '{} is sitting at floor {} and wants to reach floor {}, he is going {}'.format(self.Name,self.Location,self.Destination,self.Direction)
+        return self.__Printable   
 #===============================================================================================================================================================
 
-
+#===========================Customer class======================================================================================================================
 class Elevator(object):
-    pass
-
+    def __init__(self,PendingCalls:'List'):    
+        '''
+         Elevator receives total floor numbers and initial pending calls from the building         
+        '''
+        self.Capacity            = 0
+        self.PendingCalls        = []
+        self.Passengers          = []
+        self.DestinationsQueue   = []
+#===============================================================================================================================================================
 class Building(object):
-    pass
-
-
+    def __init__(self,CustomersNo:'int',MaxFloors:'int'):
+        pass     
 if __name__=='__main__' :
-    P = Customer('Charlie',15,6)
+    P = Customer('Charlie',15)
     print(P)
